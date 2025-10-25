022221fcd527338920f9e21af547938b04304d8e
Julio C. Barros <juliocbarros339@gmail.com>
2025-10-25 20:42:56 -0300

    Criando filtros para meus alunos

--- src/Repository/AlunoRepository.php
+++ src/Repository/AlunoRepository.php
@@ -44,4 +44,46 @@ class AlunoRepository
          d_e_l_e_t_: (bool) $dados['d_e_l_e_t_']
       );
    }
+
+   /**
+    * Busca um aluno ativo pelo seu ID.
+    */
+   public function findById(int $id): ?Aluno
+   {
+      $sql = "SELECT * FROM alunos WHERE id = ? AND d_e_l_e_t_ = false";
+
+      $stmt = $this->pdo->prepare($sql);
+      $stmt->execute([$id]);
+
+      $dados = $stmt->fetch(PDO::FETCH_ASSOC);
+
+      return $dados ? $this->hidratarAluno($dados) : null;
+   }
+
+   /**
+    * Busca um aluno ativo pelo CPF. Usado para validar a RN05.
+    */
+   public function findByCpf(string $cpf): ?Aluno
+   {
+      $sql = "SELECT * FROM alunos WHERE cpf = ? AND d_e_l_e_t_ = false";
+      $stmt = $this->pdo->prepare($sql);
+      $stmt->execute([$cpf]);
+      $dados = $stmt->fetch(PDO::FETCH_ASSOC);
+
+      return $dados ? $this->hidratarAluno($dados) : null;
+   }
+
+   /**
+    * Busca um aluno ativo pelo E-mail. Usado para validar a RN05.
+    */
+   public function findByEmail(string $email): ?Aluno
+   {
+      $sql = "SELECT * FROM alunos WHERE email = ? AND d_e_l_e_t_ = false";
+      $stmt = $this->pdo->prepare($sql);
+      $stmt->execute([$email]);
+      $dados = $stmt->fetch(PDO::FETCH_ASSOC);
+
+      return $dados ? $this->hidratarAluno($dados) : null;
+   }
+
 }
