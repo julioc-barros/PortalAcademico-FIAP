679afa0b7d4b63608dc6c244c382b8f49258ccb1
Julio C. Barros <juliocbarros339@gmail.com>
2025-10-25 11:28:00 -0300

    Instalando composer no projeto

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'fiap/portal-academico',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '44f0144852e2269a4a5812761f7225df1486b2a0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fiap/portal-academico' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '44f0144852e2269a4a5812761f7225df1486b2a0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
